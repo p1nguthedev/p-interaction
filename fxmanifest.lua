8dd6d48dc274f0628a2033adccbdeb67d39e9d59
P1ngu <97880499+p1nguthedev@users.noreply.github.com>
2024-09-01 21:58:16 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'Free open source FiveM interaction made by P1ngu'
+author 'P1ngu'
+lua54 'yes'
+version '1.0'
+legacyversion '1.9.1'
+
+client_scripts {
+	'client/client.lua'
+}
+
+export {
+	'AddInteraction',
+	'RemoveInteraction'
+}
